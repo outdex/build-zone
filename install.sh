8b073b58a04992f8bb6d5edc2e77870f18eec8d5
outdex <9071513+outdex@users.noreply.github.com>
2022-04-07 06:35:40 +0200

    Fixed small bug

--- install.sh
+++ install.sh
@@ -1,13 +1,15 @@
 #!/bin/sh
 SCRIPTPATH="$( cd "$(dirname "$0")" ; pwd -P )"
-echo $SCRIPTPATH
+
+echo "SCRIPTPATH: $SCRIPTPATH"
+
 checkrunasroot(){
   if [ ! `id -u` = 0 ] ; then
       echo "ERROR: This script must be run as the root user"
       exit 1
   fi
 }
-
+checkrunasroot
 # source ./modules/install-programs.sh
 # source ./modules/install-programs.sh
 # source ./modules/install-programs.sh
